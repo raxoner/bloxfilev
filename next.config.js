07157fdc7edde0296f30f52bc59aa04349a9ea2a
raxoner <dincoplo@gmail.com>
2025-03-09 16:23:50 +0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          { key: "X-Frame-Options", value: "DENY" },
+          { key: "X-Content-Type-Options", value: "nosniff" },
+          { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
+        ],
+      },
+    ];
+  },
+};
